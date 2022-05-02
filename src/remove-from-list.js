98036600041cfa836c3cadaafdcb878d1200a40b
Anastasia <nastia.shrai@gmail.com>
2022-05-02 22:19:27 +0300

    feat: add solution

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -22,9 +22,19 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function removeKFromList(l, k) {
+  while (l.value === k) {
+    l = l.next;
+  }
+  let node = l;
+  while (node.next) {
+    if (node.next.value === k) {
+      node.next = node.next.next;
+    } else {
+      node = node.next;
+    }
+  }
+  return l;
 }
 
 module.exports = {
